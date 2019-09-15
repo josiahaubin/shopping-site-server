e0633195adf9755d016d585e54983c373342aece
Josiah Aubin <josiahaubin@gmail.com>
2019-09-15 14:10:38 -0600

    Update Games model

--- server/services/GamesService.js
+++ server/services/GamesService.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 const _model = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
+    coverImg: { type: String, default: 'https://image.flaticon.com/icons/png/512/176/176315.png' },
     rating: { type: String, required: true },
     price: { type: Number, required: true },
     quantity: { type: Number, required: true }
