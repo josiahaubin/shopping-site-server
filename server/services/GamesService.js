697aec37c80a90da1ed049706c54633027cfbc6c
Josiah Aubin <josiahaubin@gmail.com>
2019-09-15 14:05:39 -0600

    Updated Games Model

--- server/services/GamesService.js
+++ server/services/GamesService.js
@@ -4,6 +4,9 @@ const Schema = mongoose.Schema
 const _model = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
+    rating: { type: String, required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true }
 }, { timestamps: true })
 
 export default class GamesService {
