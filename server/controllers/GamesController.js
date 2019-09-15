204f1b07670f1afd8de483ced433aaa2801b79a8
Josiah Aubin <josiahaubin@gmail.com>
2019-09-15 14:17:19 -0600

    Games routes tested

--- server/controllers/GamesController.js
+++ server/controllers/GamesController.js
@@ -4,6 +4,7 @@ import GamesService from '../services/GamesService';
 let _gamesService = new GamesService().repository
 
 export default class GamesController {
+    //All routes work
     constructor() {
         this.router = express.Router()
             //NOTE  each route gets registered as a .get, .post, .put, or .delete, the first parameter of each method is a string to be concatinated onto the base url registered with the route in main. The second parameter is the method that will be run when this route is hit.
